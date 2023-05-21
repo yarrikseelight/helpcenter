b9307dc1dd6bf001f10853b3a843563fe5bcc1c6
Jere <jereseilo@gmail.com>
2023-05-21 03:54:24 +0300

    minor changes to search bar

--- helpcenter/helpcenterapp/forms.py
+++ helpcenter/helpcenterapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(max_length=100, required=False,widget=forms.TextInput(attrs={'class': "searchfield", 'placeholder': 'Search with keyword...'}))
+    search_query = forms.CharField(max_length=100, required=False,widget=forms.TextInput(attrs={'class': "searchfield", 'placeholder': 'Enter keyword...'}))
 
 
 
--- helpcenter/helpcenterapp/urls.py
+++ helpcenter/helpcenterapp/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('help/<slug:slug>/<slug:slug2>', views.ArticleView.as_view(), name="article"),
-    path('help/<slug:slug>', views.ArticlesView.as_view(), name="articles"),
-    path('help', views.HelpcenterView.as_view(), name="helpcenter"),    
+    path('support/<slug:slug>/<slug:slug2>', views.ArticleView.as_view(), name="article"),
+    path('support/<slug:slug>', views.ArticlesView.as_view(), name="articles"),
+    path('support', views.HelpcenterView.as_view(), name="helpcenter"),    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
